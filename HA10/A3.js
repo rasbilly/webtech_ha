83a728910fb20be99741bb36dbb4a452b2f0816d
ryanmarcwegmeier <33117279+ryanmarcwegmeier@users.noreply.github.com>
2018-01-17 17:47:40 +0100

    A3_Fertig

--- HA10/A3.js
+++ HA10/A3.js
@@ -68,7 +68,9 @@ function filterTUBMails( text ){
 	if(text==null || text==undefined){
 		return []
 	}else{
-		return text.split(/\s/).filter(e=>{var p=(/[\\.a-zA-Z0-9_-]+@mailbox.tu-berlin.de/i)[Symbol.match](e);return (p!=null)?p[0].length==e.length:false}).map(e=>{var indi=(/@/ig)[Symbol.search](e);return e.substring(0,indi)+e.substring(indi,e.length).toLowerCase()})
+	//	return text.split(/\s/).filter(e=>{var p=(/[\\.a-zA-Z0-9_-]+@mailbox.tu-berlin.de/i)[Symbol.match](e);return (p!=null)?p[0].length==e.length:false}).map(e=>{var indi=(/@/ig)[Symbol.search](e);return e.substring(0,indi)+e.substring(indi,e.length).toLowerCase()})
+//		return text.split(/\s/).filter(e=>{var p=(/[\\.a-zA-Z0-9_-]+@mailbox.tu-berlin.de/i)[Symbol.match](e);return (p!=null)?p[0].length==e.length:false}).map(e=>{var indi=(/@/ig)[Symbol.search](e);return e.substring(0,indi)+e.substring(indi,e.length).toLowerCase()})
+		 return (/[\\.a-zA-Z0-9_-]+@mailbox.tu-berlin.de/ig)[Symbol.match](text).map(e=>{var indi=(/@/ig)[Symbol.search](e);return e.substring(0,indi)+e.substring(indi,e.length).toLowerCase()})
 	}
 	
 }
@@ -128,13 +130,10 @@ function getUnpublishingDate( text ){
 
 var x="2018-01-01T15:45:00.000Z"
 
-//console.log((/[0-9]+-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|3[0-1])T(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]\\.[0-9]{3}Z)/g)[Symbol.match](x))
-//console.log((/[0-9]+-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|3[0-1])T(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]\.[0-9]{3}Z/g)[Symbol.match](x))
 
-function person(){
-	
-}
-console.log(getUnpublishingDate("hallo"))
+var p=(/[\\.a-zA-Z0-9_-]+@mailbox.tu-berlin.de/ig)[Symbol.match]("hallo@mailbox.tu-berlin.de ich bin ein x+y@mailbox.tu-berlin.de und ein +@mailbox.tu.berlin.de und ein fsfs@mail.box.tu-berlin.de")
+
+console.log(filterTUBMails({test:"test"}))
 
 
 
